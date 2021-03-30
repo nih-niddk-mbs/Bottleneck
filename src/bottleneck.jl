8e7e708fd34cd8c24e229a60c758b33abe433a6e
Carson Chow <ccc1685@gmail.com>
2021-03-30 10:22:34 -0400

    updated comments

--- src/bottleneck.jl
+++ src/bottleneck.jl
@@ -18,6 +18,12 @@ R = total counts at site i for recipient m
 d = variant counts at site i for donor m
 D = total at i for donor m
 Nmax = maximum of N for sum
+
+epsilon \in{0,1} accounts for noise due to mutations or read errors
+by adding a variant in the recipient even if it was not transmitted.
+
+Posterior is a convolution of two BetaBinomial distributions
+Only include terms that are finite (i.e. do not underflow)
 """
 function likelihood_im(r,R,d,D,Nmax)
     P = zeros(Nmax,2)
@@ -33,6 +39,12 @@ function likelihood_im(r,R,d,D,Nmax)
     end
     P
 end
+"""
+loglikelihood_im(r,R,d,D,Nmax)
+
+Take log of posterior accounting for underflow
+"""
+
 
 function loglikelihood_im(r,R,d,D,Nmax)
     P = likelihood_im(r,R,d,D,Nmax)
@@ -46,27 +58,44 @@ function loglikelihood_im(r,R,d,D,Nmax)
     P
 end
 """
-likelihood_m(r,R,d,D,Nmax)
+loglikelihood_m(r,R,d,D,Nmax)
 
-Collate variant loglikelihoods of recipient-donor pair
+Sum loglikelihoods of sites for recipient-donor pair
 r = Vector of variant counts at site i for recipient
 R = Vector of total counts at site i for recipient
 d = Vector of variant counts at site i for donor
 D = Vector of total counts at site i for donor
+
 """
-function loglikelihood_m(r,R,d,D,Nmax)
+function loglikelihood_m(r::Vector{Int},R,d,D,Nmax)
     llm = zeros(Nmax,2)
     for i in eachindex(r)
         llm += loglikelihood_im(r[i],R[i],d[i],D[i],Nmax)
     end
     llm
 end
+"""
+logposterior_m(r,R,d,D,Nmax)
+
+Compute logposterior for each recipient-donor pair
+and assemble into a Vector of vectors assuming
+noninformative prior
+"""
+function logposterior_m(r::Vector{Vector},R,d,D,Nmax)
+    llm = Vector{vector}(undef,length(r))
+    for i in eachindex(r)
+        llm[i] = loglikelihood_im(r[i],R[i],d[i],D[i],Nmax)
+    end
+    llm
+end
 
 """
-loglikelihood(llm)
+logposterior(llm)
 
+Compute logposteriors of all pairs in vector llm
+using noninformative prior
 """
-function loglikelihood(llm)
+function logposterior(llm)
     ll = zeros(size(llm[1]))
     for ll in llm
         ll += llm
